30d905fd1094f38bc8878c207b7df8342f66714d
aphilip97 <33058483+aphilip97@users.noreply.github.com>
2018-03-08 01:09:01 +0000

    Add very simple validation for activities as well

--- code/production/EventCreatorApp/www/js/events/controllers/events.add.js
+++ code/production/EventCreatorApp/www/js/events/controllers/events.add.js
@@ -17,6 +17,18 @@
     });
 
     vm.save = function save() {
+      /*
+        TODO: Change the functionality of the |+ Add Activity| button in the 
+        add screen (after making the button itself as well the list and labels 
+        visible again). Change it so that it will POST the event and wait for 
+        the reponse with the eventID and when it gets the eventID it should 
+        then move onto the activity-list state having passed it the event ID 
+        which it got from the response. This will allow the full view to be 
+        utilized and by both the edit AND the add controllers whilst 
+        simultaneously getting rid of any hacky ways of hiding parts of the 
+        template and the question of whether to create separate views for both 
+        controllers to simplify things.
+      */
       eventsSrvc.postEvent(vm.event).then(
         function success(postedEvent) {
           eventsSrvc.addEvent(postedEvent);
